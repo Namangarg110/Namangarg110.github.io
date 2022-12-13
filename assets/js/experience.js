32a263ea7ddce64d9a7bb5ec80399cff0e701ca2
namangarg110 <namangarg110@gmail.com>
2022-12-14 03:17:56 +0530

    added experience page

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -14,14 +14,14 @@ const exp = [
   {
     title: "Computer Vision Intern",
     cardImage: "assets/images/experience-page/aimage.png",
-    place: "AI MAGE, Bengaluru",
+    place: "AI MAGE, Bengaluru, IN",
     time: "(Dec, 2021 - March, 2022)",
     desp: "<li>Developed a Multiclass Learning Model for Fashion Tagging and Segmentation system</li><li>Deployed on AWS Lambda and AWS Fargate for serverless worry free deployment.</li><li>Utilized docker for better compatibility of project over different developing environments</li><li>Developed a CI/CD (Continues Integration / Continues Deployment) pipeline on AWS to make easy deployment of further changes to the project</li>",
   },
   {
     title: "Jr. Machine Learning Engineer",
     cardImage: "assets/images/experience-page/omdena.png",
-    place: "Omdena",
+    place: "Omdena, Remote",
     time: "(Sep, 2021 - Dec, 2021)",
     desp: "<li>Developed a deep vision engine for rooftop solar photovoltaic analysis to improve the scalability and automation of the energy simulation platform which includes Rooftop area detection using segmentation, Obstacle detection, and Calculation of solar potential</li><li>Worked in a team of 50 members from all over the Globe and helped in four different tasks.</li><li>The model is currently being used in Helsinki City for the optimization of a solar panel placement</li>",
   },
@@ -63,28 +63,34 @@ document.addEventListener("DOMContentLoaded", showCards2);
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title: "GirlScript Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/1.jpg",
+    title: "ICETCI 2021 ML Challenge",
+    cardImage: "assets/images/experience-page/icetci2021.png",
     description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+      "Secured the third position in a computer vision competition  on “Machine Learning-based Feature Extraction of Electrical Substations from Satellite Data” with IoU of 83.24%.",
   },
   {
-    title: "StudentCode-in 2020",
-    cardImage: "assets/images/experience-page/2.jpg",
+    title: "Double Slash Hackathon",
+    cardImage: "assets/images/experience-page/slash.png",
     description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+      "Presented a methodology for sending videos to faraway places using text SMS to help rural education and secured the second runned up position in the hackathon",
   },
   {
-    title: "PClub Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/3.jpg",
+    title: "HackX by IEEE VIT Valore",
+    cardImage: "assets/images/experience-page/hackx.jpg",
     description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+      "Secured 3rd rank by Developing a Deep learning based model to detect drowsy driver and alert him",
   },
   {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
+    title: "Geo-Innovation Challenge by DST",
+    cardImage: "assets/images/experience-page/dst.jpeg",
     description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+      "Presented a proposal to benifit fishermen titled 'Pridiction of Protential Fishing Zones using marine traffic and Oceansat-2 data' and secured 1st position in the challenge",
+  },
+  {
+    title: "Exibit our work at UNWGIC 2022",
+    cardImage: "assets/images/experience-page/unwgic.jpg",
+    description:
+      "Presented our work on 'Pridiction of Protential Fishing Zones using marine traffic and Oceansat-2 data' at United Nations   World Geospatial Information Congress ",
   },
 ];
 
@@ -109,133 +115,49 @@ document.addEventListener("DOMContentLoaded", showCards);
 
 // Hackathon Section
 
-// const hackathonsection = document.querySelector(".hackathon-section");
-// const mentor = [
-//   {
-//     title: "The Uplift Project",
-//     subtitle: "Mentor",
-//     image: "assets/images/experience-page/uplift.png",
-//     desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
-//     href: "https://uplift.girlscript.tech/",
-//   },
-//   {
-//     title: "ULHacks",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/ulhacks.png",
-//     desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
-//     href: "https://ulhacks.com/",
-//   },
-//   {
-//     title: "WaffleHacks",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/wafflehacks.png",
-//     desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
-//     href: "https://wafflehacks.org/",
-//   },
-//   {
-//     title: "Elevate Tech",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/elevate.png",
-//     desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
-//     href: "https://elevatetech.codes/",
-//   },
-//   {
-//     title: "PitchTeen",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/pitchteen.png",
-//     desp: "PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
-//     href: "https://www.linkedin.com/company/pitchteen/about/",
-//   },
-//   {
-//     title: "Hack-A-Solution",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/hackasolution.png",
-//     desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
-//     href: "https://hackasolution.devpost.com/",
-//   },
-//   {
-//     title: "UniGlobe Hacks",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/uniglobe.png",
-//     desp: "UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
-//     href: "https://uniglobe-hacks.devpost.com/",
-//   },
-//   {
-//     title: "AtlasHacks",
-//     subtitle: "Mentor",
-//     image: "assets/images/experience-page/atlas.png",
-//     desp: "AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
-//     href: "https://atlashacks2.devpost.com/",
-//   },
-//   {
-//     title: "NeoHacks",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/neo.png",
-//     desp: "NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
-//     href: "https://neohacks.org/",
-//   },
-//   {
-//     title: "Mission Inspired",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/mission.png",
-//     desp: "InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
-//     href: "https://www.missioninspired.org/hackathon",
-//   },
-//   {
-//     title: "Hack3",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/hack3.png",
-//     desp: "Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
-//     href: "https://hack3.co/",
-//   },
-//   {
-//     title: "JITHack",
-//     subtitle: "Mentor",
-//     image: "assets/images/experience-page/jithack.png",
-//     desp: "JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
-//     href: "https://jithack.netlify.app/",
-//   },
-//   {
-//     title: "Recess Hacks",
-//     subtitle: "Mentor",
-//     image: "assets/images/experience-page/recess.png",
-//     desp: "Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
-//     href: "https://recesshacks.com/",
-//   },
-//   {
-//     title: "Citro Tech",
-//     subtitle: "Mentor",
-//     image: "assets/images/experience-page/citro.png",
-//     desp: "Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field</li>",
-//     href: "https://citro.tech/",
-//   },
-//   {
-//     title: "NHacks",
-//     subtitle: "Judge",
-//     image: "assets/images/experience-page/nhacks.png",
-//     desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
-//     href: "https://nhacks-vi.devpost.com/",
-//   },
-// ];
+const hackathonsection = document.querySelector(".hackathon-section");
+const mentor = [
+  {
+    title: "Indian Society for Applied Research & Development",
+    subtitle: "Data Analyst",
+    image: "assets/images/experience-page/isard.jpeg",
+    desp: "I performed data analysis on HIV data which was collected by ISARD over duration of 3 years",
+    href: "https://isardorg.in/home",
+  },
+  {
+    title: "Moksha Orphanage and Old Age Home",
+    subtitle: "Volenteer",
+    image: "assets/images/experience-page/moksha.jpg",
+    desp: "Spent time with childen and senior citizen",
+    href: "http://www.moksha-oldagehome.org/",
+  },
+  {
+    title: "Omdena",
+    subtitle: "Jr. Machine learning Engineer",
+    image: "assets/images/experience-page/omdena.png",
+    desp: "Developed a deep vision engine for rooftop solar photovoltaic analysis to improve the scalability and automation of the energy simulation platform which includes Rooftop area detection using segmentation, Obstacle detection, and Calculation of solar potential",
+    href: "https://wafflehacks.org/",
+  },
+];
 
-// const showCards3 = () => {
-//   let output = "";
-//   mentor.forEach(
-//     ({ title, image, subtitle, desp, href }) =>
-//       (output += `  
-//       <div class="blog-slider__item swiper-slide">
-//         <div class="blog-slider__img">
-//             <img src="${image}" alt="">
-//         </div>
-//         <div class="blog-slider__content">
-//           <div class="blog-slider__title">${title}</div>
-//           <span class="blog-slider__code">${subtitle}</span>
-//           <div class="blog-slider__text">${desp}</div>
-//           <a href="${href}" class="blog-slider__button">Read More</a>   
-//         </div>
-//       </div>
-//       `)
-//   );
-//   hackathonsection.innerHTML = output;
-// };
-// document.addEventListener("DOMContentLoaded", showCards3);
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp, href }) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="${href}" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
